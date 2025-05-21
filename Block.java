511310c278ba817865ac375cfefeb33145821c9d
HDuda99 <111542543+duda-honorato@users.noreply.github.com>
2025-05-21 07:14:13 -0300

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,12 +1,13 @@
+//Representa um bloco da blockchain
 import java.util.*;
 
 public class Block {
-    public int index;
-    public long timestamp;
-    public List<Transaction> transactions;
-    public String previousHash;
-    public String hash;
-    public int nonce;
+    public int index; //posicao
+    public long timestamp; //Qd criado
+    public List<Transaction> transactions; //Transacoes dentro do bloco
+    public String previousHash; //hash - bloco anterior
+    public String hash; //hash - atual
+    public int nonce; //valor - mineracao
 
     public Block(int index, List<Transaction> transactions, String previousHash) {
         this.index = index;
@@ -18,9 +19,13 @@ public class Block {
     }
 
     public String computeHash() {
+        //SHA-256 HashCriptografado
         return Utils.applySha256(index + Long.toString(timestamp) + transactions.toString() + previousHash + nonce);
+        //Ex sem SHA-256 Sem SHA-256: {1}{1716318612345}{[Alice -> Bob (10)]}{00000abc123}{3215}
+
     }
 
+    //Realiza a mineração tentando encontrar um hash que comece com difficulty zeros 
     public void mineBlock(int difficulty) {
         String target = new String(new char[difficulty]).replace('\0', '0');
         while (!hash.substring(0, difficulty).equals(target)) {
