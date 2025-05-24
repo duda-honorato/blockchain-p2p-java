13ad1b4c60a318beb0b0b2d6b88ba7d8e2ea6598
HDuda99 <111542543+duda-honorato@users.noreply.github.com>
2025-05-24 14:38:51 -0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,17 +1,23 @@
-//implementa um no da rede blockchain, capaz de relacionar as conexoes e msg a outros nos
-import java.net.*;
-import java.io.*;
+import com.google.gson.Gson;
+import java.util.*;
 
 public class Node extends Thread {
     private ServerSocket server;
-    private Blockchain blockchain; // ref - blockchain
+    private Blockchain blockchain;
+    private List<String> peers = new ArrayList<>();
+    private Gson gson = new Gson();
 
     public Node(Blockchain blockchain, int port) throws IOException {
         this.server = new ServerSocket(port);
         this.blockchain = blockchain;
     }
 
-    //fica aceitando as conexoes e lendo msg
+    public void addPeer(String ipPort) {
+        if (!peers.contains(ipPort)) {
+            peers.add(ipPort);
+        }
+    }
+
     public void run() {
         while (true) {
             try {
@@ -19,8 +25,7 @@ public class Node extends Thread {
                 BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 String line;
                 while ((line = in.readLine()) != null) {
-                    System.out.println("Mensagem recebida: " + line);
-                    // lógica de sincronização futura
+                    handleMessage(line);
                 }
                 client.close();
             } catch (IOException e) {
@@ -28,7 +33,7 @@ public class Node extends Thread {
             }
         }
     }
-    //encia msg para outros nos
+
     public void sendMessage(String ip, int port, String message) {
         try {
             Socket socket = new Socket(ip, port);
@@ -37,6 +42,28 @@ public class Node extends Thread {
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    // Envia mensagem para todos os peers
+    public void broadcast(String message) {
+        for (String peer : peers) {
+            String[] parts = peer.split(":");
+            sendMessage(parts[0], Integer.parseInt(parts[1]), message);
+        }
+    }
+
+    private void handleMessage(String msg) {
+        if (msg.startsWith("BLOCK|")) {
+            Block received = gson.fromJson(msg.substring(6), Block.class);
+            boolean added = blockchain.addBlock(received);
+            System.out.println("Bloco recebido. Adicionado? " + added);
+        } else if (msg.startsWith("TX|")) {
+            Transaction tx = gson.fromJson(msg.substring(3), Transaction.class);
+            blockchain.addTransaction(tx);
+            System.out.println("Transação recebida: " + tx);
+        } else {
+            System.out.println("Mensagem desconhecida: " + msg);
         }
     }
 }
